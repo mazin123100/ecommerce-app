55477a7e1fcc5d0b9a6ee4d80249793a5845dc54
mazin123100 <mazenalobidy@gmail.com>
2021-07-27 14:03:43 -0400

    Added product to components,homesereen to screens, images and test data (products.js)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import { Container } from "react-bootstrap"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
+import HomeScreen from "./screens/HomeScreen"
+
+
 function App() {
   return (
     <div>
       <Header />
       <main className="py-3">
-        <Container><h1>My app</h1></Container>
+        <Container><HomeScreen></HomeScreen></Container>
       </main>
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default App
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row } from "react-bootstrap"
+import { Navbar, Nav, Container } from "react-bootstrap"
 
 function Header() {
     return (
         <header>
-            <Navbar bg="dark" variant="dark" class="navbar-dark bg-primary" expand="lg" collapseOnSelect>
+            <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <Navbar.Brand href="/">Ishtar Shop</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                             <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                            <Nav.Link href="/login"> <i className="fas fa-login-user"></i>Login</Nav.Link>
-
+                            <Nav.Link href="/login"> <i className="fas fa-login"></i>Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
