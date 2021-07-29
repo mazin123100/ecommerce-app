768a1b25770b637608e45c3a5d2868c7fa2ba710
mazin123100 <mazenalobidy@gmail.com>
2021-07-29 15:19:05 -0400

    Created expenseitem component

---
+++ frontend/src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
+
+export default function ExpenseItem() {
+    return (
+        <div>
+            <h2>hi</h2>
+        </div>
+    )
+}
