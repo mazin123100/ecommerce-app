c262dff200c397ee51428375cfe0dee652d912b9
mazin123100 <mazenalobidy@gmail.com>
2021-07-29 15:01:49 -0400

    Initial commit for backend django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import ExpenseItem from './components/ExpenseItem'
 
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
@@ -15,6 +16,7 @@ function App() {
         <Container>
           <Route exact path='/' component={HomeScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/expense' component={ExpenseItem} />
         </Container>
       </main>
       <Footer />
